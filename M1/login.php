6140f28deec460147e0d3d62958cec6f97c5cc1a
SWPNL <sd2269@njit.edu>
2024-02-21 20:11:37 -0500

    Week6 Added a username, and reformatted the database, added a nav bar and it was cool overall

--- M1/login.php
+++ M1/login.php
@@ -6,6 +6,7 @@ require(__DIR__ . "/partials/nav.php");
         <label for="email">Email</label>
         <input type="email" name="email" required />
     </div>
+    
     <div>
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
--- M1/partials/nav.php
+++ M1/partials/nav.php
@@ -9,26 +9,68 @@ $localWorks = true; //some people have issues with localhost for the cookie para
 //if you're one of those people make this false
 
 //this is an extra condition added to "resolve" the localhost issue for the session cookie
-if (($localWorks && $domain == "localhost") || $domain != "localhost") {
-    session_set_cookie_params([
-        "lifetime" => 60 * 60,
-        "path" => "/Project",
-        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
-        "domain" => $domain,
-        "secure" => true,
-        "httponly" => true,
-        "samesite" => "lax"
-    ]);
-}
+// if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+//     session_set_cookie_params([
+//         "lifetime" => 60 * 60,
+//         "path" => "/Project",
+//         //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+//         "domain" => $domain,
+//         "secure" => true,
+//         "httponly" => true,
+//         "samesite" => "lax"
+//     ]);
+// }
 require_once(__DIR__ . "/../lib/functions.php");
 
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+nav li {
+  float: left;
+}
+
+nav li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+nav li a:hover {
+  background-color: #00C5FF;
+}
+</style>
+</head>
+<body>
+
+<!-- <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul> -->
+
+</body>
+</html>
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
@@ -39,3 +81,5 @@ require_once(__DIR__ . "/../lib/functions.php");
         <?php endif; ?>
     </ul>
 </nav>
+
+
